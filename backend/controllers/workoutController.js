52ca349d3c470c60baf8b8c618a6f00186d11ac0
SergiyL <s.lyubchenko@hotmail.com>
2023-07-29 20:58:46 +0300

    updated controllers

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -32,19 +32,52 @@ const createWorkout = async (req, res) => {
     // add doc to db
     try {
         const workout = await Workout.create({title, load, reps});
-        res.status(200).json(workout);
+        res.status(201).json(workout);
     } catch(err) {
         res.status(400).json({error: err.message})
     }
 }
 
 // delete a workout
+const deleteWorkout = async (req, res) => {
+    const {id} = req.params;
 
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such workout'})
+    }
+
+    const workout = await Workout.findOneAndDelete({_id: id});
+
+    if (!workout) {
+        return res.status(404).json({error: 'No such workout'})
+    }
+
+    res.status(200).json(workout);
+}
 
 // update a workout
+const updateWorkout = async (req, res) => {
+    const {id} = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such workout'})
+    }
+
+    const workout = await Workout.findOneAndUpdate({_id: id}, {
+        ...req.body
+    });
+
+    if (!workout) {
+        return res.status(404).json({error: 'No such workout'})
+    }
+
+    res.status(200).json(workout)
+}
 
 module.exports = {
     getWorkout,
     getWorkouts,
-    createWorkout
+    createWorkout,
+    deleteWorkout,
+    updateWorkout
 }
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,5 +1,11 @@
 const express = require('express');
-const { createWorkout, getWorkout, getWorkouts } = require('../controllers/workoutController');
+const { 
+    createWorkout, 
+    getWorkout, 
+    getWorkouts, 
+    deleteWorkout, 
+    updateWorkout 
+} = require('../controllers/workoutController');
 
 const router = express.Router();
 
@@ -13,13 +19,9 @@ router.get('/:id', getWorkout)
 router.post('/', createWorkout)
 
 // DELETE a new workout
-router.delete('/:id', (req, res) => {
-    res.json({mssg: 'DELETE a workout'});
-})
+router.delete('/:id', deleteWorkout)
 
 // UPDATE a new workout
-router.patch('/:id', (req, res) => {
-    res.json({mssg: 'UPDATE a workout'});
-})
+router.patch('/:id', updateWorkout)
 
 module.exports = router;
