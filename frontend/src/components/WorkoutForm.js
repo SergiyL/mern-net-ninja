a32ee0961d619320bb1b66218bab8a761a42d22f
SergiyL <s.lyubchenko@hotmail.com>
2023-07-30 11:55:30 +0300

    added workout form

---
+++ frontend/src/components/WorkoutForm.js
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+const WorkoutForm = () => {
+    const [title, setTitle] = useState('');
+    const [load, setLoad] = useState('');
+    const [reps, setReps] = useState('');
+    const [error, setError] = useState(null);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const workout = {title, load, reps}
+
+        const response = await fetch('http://localhost:4000/api/workouts', {
+            method: 'POST',
+            body: JSON.stringify(workout),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const json = await response.json();
+
+        if(!response.ok) {
+            setError(json.error);
+        }
+
+        if(response.ok) {
+            setTitle('');
+            setLoad('');
+            setReps('');
+            setError(null);
+            console.log('new workout created', json);
+        }
+    }
+
+    return (
+        <form className="create" onSubmit={handleSubmit}>
+            <h3>Додати нову вправу</h3>
+
+            <label>Назва вправи:</label>
+            <input 
+                type="text"
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+            />
+
+            <label>Навантаження (кг):</label>
+            <input 
+                type="number"
+                onChange={(e) => setLoad(e.target.value)}
+                value={load}
+            />
+
+            <label>Кількість повторень:</label>
+            <input 
+                type="number"
+                onChange={(e) => setReps(e.target.value)}
+                value={reps}
+            />
+
+            <button>Додати тренування</button>
+            {error && <div className="error">{error}</div>}
+        </form>
+    )
+}
+
+export default WorkoutForm;
