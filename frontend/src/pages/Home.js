f5ba59c20c8c4684d826d78de037a2ffac2726aa
SergiyL <s.lyubchenko@hotmail.com>
2023-07-30 18:00:41 +0300

    added context

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,25 +1,24 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
 import WorkoutDetails from '../components/WorkoutDetails';
 import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
-    const [workouts, setWorkouts] = useState(null);
+    const {workouts, dispatch} = useWorkoutsContext();
 
     useEffect(() => {
         const fetchWorkouts = async () => {
             const response = await fetch('http://localhost:4000/api/workouts');
             const json = await response.json();
 
-            console.log(response)
-
             if(response.ok) {
-                setWorkouts(json)
+                dispatch({type: 'SET_WORKOUTS', payload: json})
             }
         };
 
         fetchWorkouts();
-    }, []);
+    }, [dispatch]);
 
     return (
         <div className="home">
